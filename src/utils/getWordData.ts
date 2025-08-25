0dd09fbe068995dcb1f41504c4188e4880fc8397
Raul Galicia <raulgalicia@MacBook-Air-de-Raul.local>
2025-08-24 18:49:08 -0600

    getWordData function

--- src/utils/getWordData.ts
+++ src/utils/getWordData.ts
@@ -1,5 +1,5 @@
-export const getWordData = async () => {
-    const DICTIONARY_ENDPOINT = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+export const getWordData = async (word:string) => {
+    const DICTIONARY_ENDPOINT = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 
     const res = await fetch(DICTIONARY_ENDPOINT);
     const data = await res.json()
