0d7d661659523c6b57ff7f0950115cf1a5725509
Raul Galicia <raulgalicia@MacBook-Air-de-Raul.local>
2025-08-24 19:28:19 -0600

    InputBar logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from 'react';
 import { Header } from './components/Header';
 import { InputBar } from './components/InputBar';
 import { ResultDisplay } from './components/ResultDisplay';
+import { getWordData } from './utils/getWordData';
 
-function App() {
+const App = () => {
+
+  const [ word, setWord ] = useState('');
+  const [ wordData, setWordData ] = useState('');
+
+  useEffect(() => {
+    if (word !== '') {
+      const fetchData = async () => {
+        const data = await getWordData(word);
+        setWordData(data);
+      }
+  
+      fetchData()
+    }
+
+  },[word])
+
+  // useEffect para ver cambios en el Data del fetch
+  useEffect(() => {
+    console.log(wordData);
+  }, [wordData]);
+
+
   return (
     <main>
       <Header />
-      <InputBar />
+      <InputBar onSearch={ setWord }/>
       <ResultDisplay />
     </main>
   )
--- src/components/InputBar.tsx
+++ src/components/InputBar.tsx
@@ -1,5 +1,29 @@
-export const InputBar = () => {
-  return (
-    <input type="text" placeholder="keyboard"/>
-  )
+import { useState } from 'react';
+
+type InputBarProps = {
+  onSearch: (value: string) => void;
+};
+
+export const InputBar = ({ onSearch }: InputBarProps) => {
+
+    const [ inputValue, setInputValue ] = useState('');
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (inputValue.trim() === "") return;
+        onSearch(inputValue);
+        setInputValue('')
+    }
+    
+    return (
+        <form onSubmit={ handleSubmit }>
+            <input
+                type="text"
+                placeholder="Type a word"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button>🔎</button>
+        </form>
+    )
 }
